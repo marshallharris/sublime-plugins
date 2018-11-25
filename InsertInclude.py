b4e60597befc81a75bdf61cb7b423581a19e032c
marshallharris <marshall.harris3@gmail.com>
2018-11-24 19:23:11 -0700

    add forward declare plugin

--- InsertInclude.py
+++ InsertInclude.py
@@ -13,3 +13,13 @@ class InsertIncludeCommand(sublime_plugin.TextCommand):
     	else:
     		textToBeInsterted = '#include <' + highlightedText + '.h>\n'
     	self.view.insert(edit, indexOfNewlineFollowingLastInclude, textToBeInsterted)
+
+class InsertForwardDeclareCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        contents=self.view.substr(sublime.Region(0, self.view.size()))
+        indexOfLastInclude = contents.rfind('#include')
+        indexOfNewlineFollowingLastInclude = contents.find('\n', indexOfLastInclude) + 1
+        highlightedText = self.view.substr(self.view.sel()[0])
+        textToBeInsterted = ''
+        textToBeInsterted = 'class ' + highlightedText + ';\n'
+        self.view.insert(edit, indexOfNewlineFollowingLastInclude, textToBeInsterted)
